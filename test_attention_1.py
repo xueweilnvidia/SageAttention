0f213762bd8a0e5e2ab87964b3404c273d300f3f
xueweil <xueweil@nvidia.com>
2024-12-22 21:26:51 -0800

    minor

--- test_attention_1.py
+++ test_attention_1.py
@@ -4,34 +4,37 @@ import torch.nn.functional as F
 from datetime import datetime
 from einops import rearrange, repeat
 
+import flash_attn
+from flash_attn.flash_attn_interface import _flash_attn_forward
+from flash_attn.flash_attn_interface import flash_attn_varlen_func, flash_attn_func
+
 torch.manual_seed(81627)
 torch.set_printoptions(precision=4, threshold=None, edgeitems=None, linewidth=None, profile=None, sci_mode=False)
 
-batch_size = 3
+batch_size = 1
 head_num = 3
-seq_len = 119056
+seq_len = 118824
 # seq_len = 512
 head_dim = 128
 
-q = torch.randn((batch_size, head_num, seq_len, head_dim), dtype=torch.bfloat16).cuda()*5
-k = torch.randn((batch_size, head_num, seq_len, head_dim), dtype=torch.bfloat16).cuda()*5
-v = torch.randn((batch_size, head_num, seq_len, head_dim), dtype=torch.bfloat16).cuda()*5
-
-for i in range(1):
-    attn_output = F.scaled_dot_product_attention(q, k, v)
+q = torch.randn((batch_size,  seq_len,head_num, head_dim), dtype=torch.bfloat16).cuda()
+k = torch.randn((batch_size,  seq_len,head_num, head_dim), dtype=torch.bfloat16).cuda()
+v = torch.randn((batch_size,  seq_len,head_num, head_dim), dtype=torch.bfloat16).cuda()
 
 
-print("attn_output",attn_output)
-
-
-
-for i in range(1):
+for i in range(3):
     # attn_output1 = sageattn_qk_int8_pv_fp8_cuda(q, k, v, tensor_layout="HND", is_causal=False, pv_accum_dtype="fp32+fp32")
-    attn_output1 = sageattn_qk_int8_pv_fp16_triton(q, k, v, tensor_layout="HND", is_causal=False)
+    attn_output = sageattn_qk_int8_pv_fp16_triton(q.clone().transpose(1,2).contiguous(), k.clone().transpose(1,2).contiguous(), v.clone().transpose(1,2).contiguous(), tensor_layout="HND", is_causal=False)
+    attn_output = attn_output.transpose(1,2)
     # attn_output1 = sageattn_varlen(q, k, v, cu_seqlen, cu_seqlen, max_seqlen, max_seqlen, is_causal=False)
+print("attn_fp8: ", attn_output)
+
+for i in range(1):
+    # attn_output1 = F.scaled_dot_product_attention(q, k, v, is_causal=False)
+    attn_output1 = flash_attn_func(q, k, v)
+print("attn_output",attn_output1)
 
 
-print("attn_fp8: ", attn_output1)
 
 
 attn_output = attn_output.to(torch.float32).reshape(-1)
